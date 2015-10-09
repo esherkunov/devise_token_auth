14fe8002a7badba240b2f55dcd29d25a9d86b186
Evgeny Sherkunov <esherkunov@gmail.com>
2015-10-09 10:54:52 +0300

    Set provider to auth key field name in session create process to let user sign in with any auth key.

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -20,7 +20,7 @@ module DeviseTokenAuth
           q_value.downcase!
         end
 
-        q = "#{field.to_s} = ? AND provider='phone'"
+        q = "#{field.to_s} = ? AND provider='#{field}'"
 
         if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'mysql'
           q = "BINARY " + q
